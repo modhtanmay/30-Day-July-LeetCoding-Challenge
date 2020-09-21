0d9f120c4b08601598bb16deae303794fb16bf60
modhtanmay <modhtanmay@gmail.com>
2020-09-22 00:55:57 +0530

    edited files

--- Day24-All-Paths-From-Source-to-Targe.java
+++ Day24-All-Paths-From-Source-to-Targe.java
@@ -26,11 +26,12 @@ class Solution {
         dfs(graph, 0, graph.length - 1, new ArrayList(), result);
         return result;
     }
-    
+
     private void dfs(int[][] graph, int source, int destination, List<Integer> list, List<List<Integer>> result) {
-        if(source == destination) result.add(list); // if soruce reached destination add that path in result
+        if (source == destination)
+            result.add(list); // if soruce reached destination add that path in result
         list.add(source); // add current vertex
-        for(Integer vertex : graph[source]) {
+        for (Integer vertex : graph[source]) {
             List<Integer> copyList = new ArrayList<>(list); // clone the path
             dfs(graph, vertex, destination, copyList, result);
         }
